e4d0c286cf8564aa939e0f9688efd225f6cc03f3
Rares Rusu <rares.rusu@paddypowerbetfair.com>
2021-02-02 18:34:45 +0200

    add controller

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # gyrus
 An attempt to create a system that handles messages, notifications, data for users. Much like a rotating ocean current.
+
+# deployment
+
+Heroku - configured to pull content from Github
+
+https://dashboard.heroku.com/apps/gyrus-app/settings
+
+# URLs
+
+Applicatiom URL on Heroku
+https://gyrus-app.herokuapp.com/actuator/health
+
+Upload file endpoint
+curl -v -F file=@ticket.txt http://localhost:8080/api/upload
+
+
+# AWS S3 for storage
+
+## General details
+accesible from Heroku
+Pricing: Standard - Infrequent access
+
+AWS Free Tier - 1 year
+
+Amazon Simple Storage Service (S3) is a “highly durable and available store” that is ideal for storing application content such as media files, static assets, and user uploads.
+
+## File uploads
+There are two approaches to processing and storing file uploads from a Heroku app to S3: direct and pass-through.
+
+## AWS S3 details
+The object key (or key name) uniquely identifies the object in an Amazon S3 bucket. Object metadata is a set of name-value pairs.
+The Amazon S3 data model is a flat structure: You create a bucket, and the bucket store objects. There is no hierarchy of subbuckets or subfolders. However, you can infer logical hierarchy using key name prefixes and delimiters as the Amazon S3 console does.
+The Amazon S3 console supports a concept of folders.
+
+You can set object metadata in Amazon S3 at the time you upload the object. After you upload the object, you cannot modify object metadata. The only way to modify object metadata is to make a copy of the object and set the metadata.
+
+You can have an unlimited number of objects in a bucket.
+
+# Logging
+
+## Log DNA
+
+https://app.logdna.com/b4a7f08034/logs/view
--- src/main/java/com/gyrus/UploadController.java
+++ src/main/java/com/gyrus/UploadController.java
@@ -1,20 +1,46 @@
 package com.gyrus;
 
+import java.io.IOException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @RestController
 @RequestMapping("/api")
 public class UploadController {
 
+    private static final String S3_BUCKET = "gyrus";
+
 
     @PostMapping("/upload")
-    public ResponseEntity<Response> upload() {
+    public RedirectView upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 
-        return ResponseEntity.ok().build();
+        S3Client s3Client = S3Client.builder()
+            .region(Region.EU_WEST_1)
+            .build();
+
+
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+            .bucket(S3_BUCKET)
+            .key(file.getName())
+            .build();
+        try {
+            s3Client.putObject(objectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename());
+
+        return new RedirectView("/success", true);
     }
-
-
 }
