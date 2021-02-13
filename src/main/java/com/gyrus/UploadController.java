c6e2f52c74f7720188dee3c2a3e0fbf9260fa5ce
Rares Rusu <rares.rusu@paddypowerbetfair.com>
2021-02-13 10:01:34 +0200

    refactor

--- src/main/java/com/gyrus/UploadController.java
+++ src/main/java/com/gyrus/UploadController.java
@@ -1,7 +1,7 @@
 package com.gyrus;
 
-import java.io.IOException;
-import org.springframework.http.ResponseEntity;
+import com.gyrus.s3.S3Delegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,39 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @RestController
 @RequestMapping("/api")
 public class UploadController {
 
-    private static final String S3_BUCKET = "gyrus-bucket";
-
+    @Autowired
+    private S3Delegate s3Delegate;
 
     @PostMapping("/upload")
     public RedirectView upload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-
-        S3Client s3Client = S3Client.builder()
-            .region(Region.EU_WEST_1)
-            .credentialsProvider(ProfileCredentialsProvider.create())
-            .build();
-
-        System.out.println(s3Client.listBuckets().buckets().get(0).name());
-
-        PutObjectRequest objectRequest = PutObjectRequest.builder()
-            .bucket(S3_BUCKET)
-            .key(file.getName())
-            .build();
-        try {
-            s3Client.putObject(objectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        s3Delegate.upload(file);
         redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename());
 
         return new RedirectView("/success", true);
---
+++ src/main/java/com/gyrus/s3/S3Delegate.java
@@ -0,0 +1,42 @@
+package com.gyrus.s3;
+
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Component
+public class S3Delegate {
+
+    private static final String S3_BUCKET = "gyrus-bucket";
+
+    private Logger LOG = LoggerFactory.getLogger(S3Delegate.class);
+
+    private S3Client s3Client;
+
+    public S3Delegate() {
+        s3Client = S3Client.builder()
+            .region(Region.EU_WEST_1)
+            .credentialsProvider(ProfileCredentialsProvider.create())
+            .build();
+    }
+
+    public void upload(MultipartFile file) {
+        LOG.info("Uploading file " + file.getOriginalFilename());
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+            .bucket(S3_BUCKET)
+            .key(file.getName())
+            .build();
+        try {
+            s3Client.putObject(objectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
